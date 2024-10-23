1c54714ddb3ec87a90f2407596655908a962671c
viktorsvider <viktor1svider@gmail.com>
2024-10-23 19:45:53 +0000

    props usage advanced

--- examples/part1/src/App.jsx
+++ examples/part1/src/App.jsx
@@ -1,17 +1,23 @@
 const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name='George' />
-      <Hello name='Daisy' />
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
